4b1f0a79adb91dfe6f51a87ded0efc5137ea8922
FRCTeam360 <programmersFRC360@gmail.com>
2015-01-29 17:17:42 -0800

    add code

--- RainMakerXVI/src/org/usfirst/frc/team360/robot/Robot.java
+++ RainMakerXVI/src/org/usfirst/frc/team360/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Compressor;
 
 /**
@@ -20,11 +21,10 @@ import edu.wpi.first.wpilibj.Compressor;
 //tank drive 2 Joysticks, left trigger engages down speed right trigger activates up speed
 public class Robot extends IterativeRobot {
 	RobotDrive myRobot;
-	public static Joystick stickR;
-	public static Joystick stickL;
-	public static Joystick gamePad; 
-	//public static double joyR = stickR.getRawAxis(1);
-	//public static double joyL = stickL.getRawAxis(1);
+	 Joystick stickR;
+	 Joystick stickL;
+	 Joystick gamePad; 
+	
 	
 	
 	
@@ -34,18 +34,17 @@ public class Robot extends IterativeRobot {
 	boolean release; 
 	boolean up;
     boolean down;
-	//Button buttonShift= new JoystickButton(stickL, 1);
 	double stickPos = 1;
-	double joyR = stickR.getRawAxis(1);
-	double joyL = stickL.getRawAxis(1);
-	//double intake = gamePad.getRawAxis(2);
+	//double joyR = stickR.getRawAxis(1);
+	//double joyL = stickL.getRawAxis(1);
+	//double Intake = gamePad.getRawAxis(2);
 	public static boolean halfSpeed;
+	DoubleSolenoid intakeSol = new DoubleSolenoid(0, 1);
+	public Compressor compressor = new Compressor(0);//init compressor and maps it
 	
-	DoubleSolenoid intake = new DoubleSolenoid(8, 1, 2);
 	
-	//Compressor = new Compressor(0);
-	public Compressor compressor = new Compressor(0);
-	//SetClosedLoopControl(false);
+
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -58,77 +57,15 @@ public class Robot extends IterativeRobot {
     	myRobot.setExpiration(0.1);
     	stickR = new Joystick(1); 
     	stickL = new Joystick(0);
-    	//SetClosedLoopControl(false);
-    	//halfSpeed = true;
-//    	Button buttonUp = new JoystickButton(stickR, 1);
-//       Button buttonDown = new JoystickButton(stickL, 1);
-        
-        
+    	gamePad = new Joystick(2);
+    	halfSpeed = true; 
         
     }
-   /*
-    
-    	
-
-        	
-        	
-        	//double joyR = stickR.getRawAxis(1);
-        	//double joyL = stickL.getRawAxis(1);
-             ?	
-        	stickPos = 1;
-        	/*
-        	if (stickPos <= .5 && stickPos >= .2){//checks to see is it is under 1/2 power so motor doesn't burn itself out	
-        		//joyR *= 2;//double speed again
-        		stickPos *= 2;
-        		//myRobot.drive(joyL, joyR);
-        		System.out.println("up");
-        		myRobot.tankDrive(0.9, 0.9);
-        	} 
-        	*/
-    /*
-        if (stickPos >= .5 && stickPos <= 1){//checks to see is it is over 1 power so motor doesn't get REALLY low power
-           	//joyR *= 2;//half speed again
-           	stickPos *= 2;
-           	myRobot.tankDrive(0.9, 0.9);
-           	System.out.println("up");
-           	halfSpeed = true;
-            	
-            }
-	}
    
-    
-    public void shiftDown(){
-    	
-        	
-        
-        if (stickPos > .5 && stickPos <= 1){//checks to see is it is over 1 power so motor doesn't get REALLY low power
-       	//joyR *= .5;//half speed again
-       	stickPos *= .5;
-       	myRobot.tankDrive(0.8, 0.8);
-      	System.out.println("down");
-        	
-       } 
-        		
-        }
-    
-    /**
-     * This function is run once each time the robot enters autonomous mode
-     */
     public void disabledInit() {
     	compressor.stop();
     	//set percent to 0
 
-      /*driveBase.stopMotor();
-        driveLift.stopMotor();
-        arm.set(0);
-        //set launchCatch to closed
-        launchCatch.set(0.0);
-        //stop the compressor
-        compressor.stop();
-        //stop the encoders
-        encLeft.stop();
-        encRight.stop();
-        encLift.stop();*/
     }
     
     public void autonomousInit() {
@@ -161,13 +98,13 @@ public class Robot extends IterativeRobot {
         down = stickL.getRawButton(1);
         grab = gamePad.getRawButton(1);
         release = gamePad.getRawButton(2);
-       // double joyR = stickR.getRawAxis(1);
-       // double joyL = stickL.getRawAxis(1);
-    	//double joy3 = GamePad.getRawAxis(1); 
+        double joyR = stickR.getRawAxis(1);
+        double joyL = stickL.getRawAxis(1);
+    	double joy3 = gamePad.getRawAxis(1); 
     	//myRobot.tankdrive(joyR, joyL);
         
       	//myRobot.tankDrive(-joyR, joyL); 
-      	
+      	intakeControl();
            Timer.delay(0.005); // wait for a motor update time
         if(up == true && down == false) {
         	//shiftUp();
@@ -204,24 +141,17 @@ public class Robot extends IterativeRobot {
     		grab = gamePad.getRawButton(7);
     		release = gamePad.getRawButton(8);
             //Claw control
-            if (grab)
+            if (grab==true)
             {
-                intake.set(DoubleSolenoid.Value.kForward);//grab
+                intakeSol.set(DoubleSolenoid.Value.kForward);//grab
+                System.out.println("speed = reverse");
             } 
-            else if (release) 
+            else if (release==true) 
             {
-                intake.set(DoubleSolenoid.Value.kReverse);//release
+                intakeSol.set(DoubleSolenoid.Value.kReverse);//release
+                System.out.println("speed = forward");
             }
    }
-    /*
-    public void operatorControl() {
-        myRobot.setSafetyEnabled(true);
-        while (isOperatorControl() && isEnabled()) {    // don't use, its command based not iterative
-       myRobot.tankDrive(stickR, stickL);
-           Timer.delay(0.005);		// wait for a motor update time
-       }
-    }
-    */
     
     /**
      * This function is called periodically during test mode
